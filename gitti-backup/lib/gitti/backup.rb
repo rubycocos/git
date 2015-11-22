778248916ea5b799b656a1653fd29c94eac234ba
Gerald Bauer <gerald.bauer@gmail.com>
2015-11-22 11:17:40 +0100

    mv reposet

--- gitti-backup/lib/gitti/backup.rb
+++ gitti-backup/lib/gitti/backup.rb
@@ -5,7 +5,6 @@ require 'gitti'
 # our own code
 require 'gitti/backup/version'   # note: let version always go first
 require 'gitti/backup/repo'
-require 'gitti/backup/reposet'   ##  todo: move to gitti-support (commons) gem ??
 require 'gitti/backup/backup'
 
 
